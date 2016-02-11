2e0b610da5f047459db29d5c645ff4dafe0fb2ab
tarou mame <mametarou963@outlook.jp>
2016-02-11 18:33:57 +0900

    make simple graph.js

--- makeCTCapViewFile.rb
+++ makeCTCapViewFile.rb
@@ -1,6 +1,42 @@
-print "hello World"
+#graph.js用のテキスト(固定)
+graph_js_text = <<"EOS"
+var lineChartData = {
+  labels : [],
+  datasets : [
+    {
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,1)",
+      pointColor : "rgba(220,220,220,1)",
+      pointStrokeColor : "#fff",
+      data : []
+    }
+  ]
+}
+var myLine = new Chart(document.getElementById("line").getContext("2d")).Line(lineChartData);
+EOS
 
-f = open("MF-CT1 Ver5.20/MF-CT1.map")
-f.each{|line| print line}
-f.close
+#main
+#■graph.jsの作成(テンプレート作成)
+File.open("graph.js","w") do |file| file.puts(graph_js_text) end
 
+
+#■mapファイルからDバージョンとサイズの抽出
+# D行の次にサイズが書いてあるのでその部分を抽出する
+verAry = Array.new
+motSizeArr = Array.new
+f = open("Ver5.20/MF-CT1.map") #ファイルオープン
+f.each do |line|
+  if line =~ /^D/ then
+    motSizeArr.push(f.readline.split(" ")[1])
+    break
+  end
+end
+f.close #ファイルクローズ
+
+#■サイズの抽出
+p motSizeArr
+p "0x400000".to_i(16)
+
+
+#■
+p Dir.glob('*/*.map')
