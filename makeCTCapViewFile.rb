25c8b66c5c99730d91c5e23843a4de76e516fc84
tarou mame <mametarou963@outlook.jp>
2016-02-12 07:30:26 +0900

    tinny modify

--- makeCTCapViewFile.rb
+++ makeCTCapViewFile.rb
@@ -1,3 +1,5 @@
+#実行するとmotファイルを100%として各バージョンの容量がchart.js用に吐き出される
+
 #graph.js用のテキスト(固定)
 graph_js_text = <<"EOS"
 var lineChartData = {
@@ -16,27 +18,44 @@ var myLine = new Chart(document.getElementById("line").getContext("2d")).Line(li
 EOS
 
 #main
-#■graph.jsの作成(テンプレート作成)
-File.open("graph.js","w") do |file| file.puts(graph_js_text) end
-
-
 #■mapファイルからDバージョンとサイズの抽出
 # D行の次にサイズが書いてあるのでその部分を抽出する
 verAry = Array.new
 motSizeArr = Array.new
-f = open("Ver5.20/MF-CT1.map") #ファイルオープン
-f.each do |line|
-  if line =~ /^D/ then
-    motSizeArr.push(f.readline.split(" ")[1])
-    break
-  end
-end
-f.close #ファイルクローズ
 
-#■サイズの抽出
+#■各フォルダにアクセス
+Dir.glob('*/*.map'){ |file|
+  f = open(file) #ファイルオープン
+  f.each do |line|
+    if line =~ /^D/ then
+      verAry.push(file.split("/")[-2])
+      motSizeArr.push(f.readline.split(" ")[1])
+      break
+    end
+  end
+  f.close #ファイルクローズ
+}
+
+#■バージョンには"が必要なためあらかじめ足しておく
+verAry.map!{ |item| "\"" + item + "\""}
+#■サイズはパーセント表記に修正しておく
+motSizeArr.map!{ |item| item.to_i(16).to_f/"0x400000".to_i(16).to_f }
+#■ファイルサイズは予め整数になおしておく
+
+
+#■graph.jsを作成しlabelとdataを入力
+
+#■graph.jsの作成(テンプレート作成)
+graph_js_text.gsub!('labels : []','labels : ['+ verAry.join(',') +']')
+graph_js_text.gsub!('data : []','data : [' + motSizeArr.join(',') + ']')
+File.open("graph.js","w") do |file|
+  file.puts(graph_js_text)
+end
+
+File.open("test.js","w") do |file|
+  file.puts('[' + verAry.join(',') + ']')
+  file.puts
+end
+p verAry.join('aaa')
 p motSizeArr
 p "0x400000".to_i(16)
-
-
-#■
-p Dir.glob('*/*.map')
